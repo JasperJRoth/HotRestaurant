32302066fc286c0f1900b403db45d8f4ff737027
Carolina Cavalcanti <carolinapc@gmail.com>
2019-08-17 11:09:16 -0400

    Server created with all basic functionalities

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
 const express = require("express");
-const path = require("path")
+const path = require("path");
 
 const app = express();
+
+var tables = [];
+var waitList = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "index.html"));
 });
+
+app.get("/tables", function(req, res){
+    res.sendFile(path.join(__dirname, "view.html"));
+});
+
+app.get("/reserve", function(req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.post("/reserve", function (req, res) {
+    var reservation = req.body;
+
+    if (tables.length < 5) {
+        tables.push(reservation);   
+        console.log("Reservation added to tables list");
+    }
+    else {
+        waitList.push(reservation);
+        console.log("Reservation added to wait list");
+    }
+    
+    res.json(reservation);
+});
+
+app.get("/api/tables", function(req, res){
+    res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res){
+    res.json(waitList);
+});
+
 
 app.listen(8080);
